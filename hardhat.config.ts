5803ecb1982acdec52e2b7b3a8c02366d244187b
BeroBurny <bero4net@gmail.com>
2024-02-12 10:38:01 +0100

    s

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,14 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@chainsafe/hardhat-plugin-multichain-deploy";
+import {HardhatUserConfig, task} from "hardhat/config";
 import "@nomifoundation/hardhat-web3-v4";
+import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
 };
 
 export default config;
+
+
+task("accounts", "Prints accounts", async (_, { web3, ethers }) => {
+  console.log(await web3.eth.getAccounts());
+});
